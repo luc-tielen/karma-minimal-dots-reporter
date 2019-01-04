baf0be2112a4d48e5b9d8c70a3266d9cf55fa96c
Luc Tielen <luc.tielen@kabisa.nl>
2019-01-04 14:59:01 +0100

    Take user config into account for success and fail symbols

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-var MinimalDotsReporter = function(baseReporterDecorator) {
+var defaultConfig = {
+  success: ".",
+  failure: "x"
+};
+
+var MinimalDotsReporter = function(baseReporterDecorator, config) {
   baseReporterDecorator(this);
+  var cfg = Object.assign(defaultConfig, config.minDotsReporter || {});
 
   this.specSuccess = function() {
-    process.stdout.write(".");
+    process.stdout.write(cfg.success);
   };
 
   this.specFailure = function() {
-    process.stdout.write("x");
+    process.stdout.write(cfg.failure);
   };
 
   this.onBrowserLog = function() {}; // no-op
 };
 
-MinimalDotsReporter.$inject = ["baseReporterDecorator"];
+MinimalDotsReporter.$inject = ["baseReporterDecorator", "config"];
 
 module.exports = {
   "reporter:min-dots": ["type", MinimalDotsReporter]
